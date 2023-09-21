dbe9cf0f420f52b8dd8c39dff8b2de086f091b8c
Alex Zub <killdamusic@mail.ru>
2023-09-21 23:27:13 +0300

    change flake8

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from os import path as ospath, getenv
+from os import getenv
+from os import path as ospath
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
