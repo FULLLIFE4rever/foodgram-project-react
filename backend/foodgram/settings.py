33aac51d85aa70c67a9633b045e54b85c0c3da95
Alex Zub <killdamusic@mail.ru>
2023-09-27 08:04:40 +0300

    fasd

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -84,14 +84,8 @@ WSGI_APPLICATION = "foodgram.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
-"""DATABASES = {
+DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": getenv("POSTGRES_DB", "django"),
@@ -100,7 +94,7 @@ DATABASES = {
         "HOST": getenv("DB_HOST", ""),
         "PORT": getenv("DB_PORT", 5432),
     }
-}"""
+}
 
 
 # Password validation
